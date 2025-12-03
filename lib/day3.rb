3032f044151fa96e6e892e6394c238b788317809
Aaron Swerlein <aaron.swerlein@safelite.com>
2025-12-03 08:12:08 -0500

    small tweaks

--- lib/day3.rb
+++ lib/day3.rb
@@ -6,29 +6,31 @@ class PowerBank
     @total_power = 0
   end
 
-  def find_max_for_row_using(row, count)
+  # refactored to allow single method with second parameter for number of batteries to use
+  def find_max_for_row_using(row, num_of_batteries)
     stack = []
-    max_val = 0
-    batter_to_remove = row.size - count
-    row.each do |digit|
-      while !stack.empty? && batter_to_remove > 0 && stack[-1] < digit
+    max = 0
+    batteries_to_remove = row.size - num_of_batteries
+    row.each do |num|
+      while !stack.empty? && batteries_to_remove > 0 && stack[-1] < num
         stack.pop
-        batter_to_remove -= 1
+        batteries_to_remove -= 1
       end
-      stack << digit
+      stack << num
     end
-    max_val = stack[0, count].join.to_i
-    @total_power += max_val
+    max = stack[0, num_of_batteries].join.to_i
+    @total_power += max
   end
 
 end
 
 if __FILE__ == $0
+  count = 12 # change to 2 for part 1
   batteries = File.readlines('lib\data\day3.txt').map(&:chomp)
   power = PowerBank.new()
   batteries.each do |grid|
-    power.find_max_for_row_using(grid.chars.map(&:to_i), 12)
+    power.find_max_for_row_using(grid.chars.map(&:to_i), count)
   end
 
-  puts "Total power: #{power.total_power}"
+  puts "Total power for #{count} batteries: #{power.total_power}"
 end
